74db345e9c0841ee3149513aad0e5054847d9a90
Your Name <your-email@example.com>
2026-02-26 11:59:54 +0530

    Add Jest config and coverage report

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.js'],
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  testPathIgnorePatterns: ['node_modules'],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 70,
+      functions: 80,
+      lines: 80,
+    },
+  },
+};
